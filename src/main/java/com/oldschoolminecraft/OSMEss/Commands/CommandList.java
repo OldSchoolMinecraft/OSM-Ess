9ff0c183106cb4db11f5fc77f8dbc5bd949e81f9
moderatorman <12193288+moderatorman@users.noreply.github.com>
2025-10-16 10:35:45 -0700

    Group-based list impl

--- src/main/java/com/oldschoolminecraft/OSMEss/Commands/CommandList.java
+++ src/main/java/com/oldschoolminecraft/OSMEss/Commands/CommandList.java
@@ -9,8 +9,10 @@ import org.bukkit.entity.Player;
 import org.json.simple.JSONObject;
 import ru.tehkode.permissions.PermissionGroup;
 import ru.tehkode.permissions.PermissionUser;
+import ru.tehkode.permissions.bukkit.PermissionsEx;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
@@ -33,15 +35,38 @@ public class CommandList implements CommandExecutor {
         if (cmd.getName().equalsIgnoreCase("list") || cmd.getName().equalsIgnoreCase("online")) {
             StringBuilder stringBuilder = new StringBuilder();
 
+            ConcurrentHashMap<PermissionGroup, ArrayList<PermissionUser>> groups = new ConcurrentHashMap<>();
+
             if (plugin.isPermissionsExEnabled()) { //Use grouping ranks if PermissionsEx exists.
-                Map<PermissionGroup, ArrayList<PermissionUser>> groups = new ConcurrentHashMap<>();
-                List<Player> players = new ArrayList<>();
+                for (Player onlinePlayer : Bukkit.getOnlinePlayers())
+                {
+                    PermissionUser pexUser = PermissionsEx.getPermissionManager().getUser(onlinePlayer);
+                    PermissionGroup pexGroup = pexUser.getGroups()[0];
 
-                //Todo: methods of organizing player's by ranks.
+                    groups.get(pexGroup).add(pexUser);
+                }
 
+                String listHeader = "§7There are §8" + Bukkit.getServer().getOnlinePlayers().length + " §7out of a maximum §8" + Bukkit.getServer().getMaxPlayers() + " §7players online.";
+                stringBuilder.append(listHeader);
 
-                sender.sendMessage("§7There are §8" + Bukkit.getServer().getOnlinePlayers().length + " §7out of a maximum §8" + Bukkit.getServer().getMaxPlayers() + " §7players online.");
-                sender.sendMessage(stringBuilder.toString());
+                for (PermissionGroup group : groups.keySet())
+                {
+                    stringBuilder.append("\n§7").append(group.getName()).append("§7: ");
+                    int userIndex = 0;
+                    for (PermissionUser user : group.getUsers())
+                    {
+                        userIndex++;
+                        stringBuilder.append("§8").append(user.getName());
+                        if (userIndex < group.getUsers().length)
+                            stringBuilder.append("§7, ");
+                    }
+                }
+
+                String finalOut = stringBuilder.toString();
+                finalOut = removePrefix(finalOut, "\n");
+                finalOut = removeSuffix(finalOut, "\n");
+
+                sendMultiline(sender, finalOut);
                 return true;
             }
             else { //Regular list of players. Fallback option if PermissionsEx is not installed.
@@ -72,7 +97,21 @@ public class CommandList implements CommandExecutor {
         return true;
     }
 
+    public static String removePrefix(String str, String prefix) {
+        if (str == null || prefix == null) return str;
+        return str.startsWith(prefix) ? str.substring(prefix.length()) : str;
+    }
 
+    public static String removeSuffix(String str, String suffix) {
+        if (str == null || suffix == null) return str;
+        return str.endsWith(suffix) ? str.substring(0, str.length() - suffix.length()) : str;
+    }
+
+    private static void sendMultiline(CommandSender sender, String message) {
+        String[] lines = message.split("\n");
+        for (String line : lines)
+            sender.sendMessage(line);
+    }
 
     private static String capitalize(String str) {
         if (str == null || str.isEmpty()) {
