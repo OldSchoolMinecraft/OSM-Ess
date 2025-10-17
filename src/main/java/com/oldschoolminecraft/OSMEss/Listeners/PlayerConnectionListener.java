df670cc6254436c505f9b10fde8dd6af70089e95
SavageUser <LimitlessPowerHD@gmail.com>
2025-10-17 02:44:43 -0400

    Create PlayerConnectionListener.java

---
+++ src/main/java/com/oldschoolminecraft/OSMEss/Listeners/PlayerConnectionListener.java
@@ -0,0 +1,43 @@
+package com.oldschoolminecraft.OSMEss.Listeners;
+
+import com.oldschoolminecraft.OSMEss.OSMEss;
+import org.bukkit.Bukkit;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.event.player.PlayerListener;
+import org.bukkit.event.player.PlayerQuitEvent;
+
+public class PlayerConnectionListener extends PlayerListener {
+
+    public OSMEss plugin;
+
+    public PlayerConnectionListener(OSMEss plugin) {
+        this.plugin = plugin;
+    }
+
+//    @EventHandler
+//    public void on(PlayerPreLoginEvent event) {}
+//
+//    @EventHandler
+//    public void on(PlayerLoginEvent event) {}
+
+    @EventHandler
+    public void onJoin(PlayerJoinEvent event) {
+        Player player = event.getPlayer();
+
+        if (!plugin.playerDataHandler.hasData(player)) {
+            plugin.playerDataHandler.createData(player);
+        }
+        else {
+            plugin.playtimeHandler.updateLastLogin(player);
+            Bukkit.getServer().getLogger().info("[OSM-Ess] Retrieved data for " + player.getName() + "! (Filename: " + player.getName().toLowerCase() + ".json)");
+        }
+    }
+
+    @EventHandler
+    public void onLeave(PlayerQuitEvent event) {
+        Player player = event.getPlayer();
+        plugin.playtimeHandler.updateTotalPlaytime(player);
+    }
+}
