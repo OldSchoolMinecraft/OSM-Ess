b40f3fc8a0b120fbd5acd0730e71e94f1b6fbd06
SavageUser <LimitlessPowerHD@gmail.com>
2025-10-21 17:34:17 -0400

    Added methods for updating and checking for 'ignoreBroadcast'

--- src/main/java/com/oldschoolminecraft/OSMEss/Handlers/PlayerDataHandler.java
+++ src/main/java/com/oldschoolminecraft/OSMEss/Handlers/PlayerDataHandler.java
@@ -1,15 +1,13 @@
 package com.oldschoolminecraft.OSMEss.Handlers;
 
 import com.oldschoolminecraft.OSMEss.OSMEss;
+import com.oldschoolminecraft.OSMEss.compat.OSMPLUserData;
 import org.bukkit.Bukkit;
 import org.bukkit.OfflinePlayer;
 import org.bukkit.entity.Player;
 import org.json.simple.JSONObject;
 
-import java.io.File;
-import java.io.FileWriter;
-import java.io.IOException;
-import java.io.Writer;
+import java.io.*;
 
 public class PlayerDataHandler {
 
@@ -34,6 +32,7 @@ public class PlayerDataHandler {
             jsonObject.put("lastLogOut", 0);
             jsonObject.put("playTime", 0);
             jsonObject.put("firstJoin", System.currentTimeMillis());
+            jsonObject.put("ignoreBroadcast", false);
 
             Writer writer = new FileWriter(file, false);
             writer.write(jsonObject.toString());
@@ -56,4 +55,51 @@ public class PlayerDataHandler {
         else return false;
     }
 
+    public void updateIgnoreBroadcast(Player player, boolean option) {
+        try (FileReader reader = new FileReader(new File(plugin.getDataFolder().getAbsolutePath() + "/player-logs", player.getName().toLowerCase() + ".json"))) {
+            OSMPLUserData data = OSMPLUserData.gson.fromJson(reader, OSMPLUserData.class);
+            JSONObject jsonObject = new JSONObject();
+
+            //Retain
+            long lastLoginRETAIN = data.lastLogIn;
+            long lastLogoutRETAIN = data.lastLogOut;
+            long totalPlayTimeRETAIN = data.playTime;
+            long firstJoinRETAIN = data.firstJoin;
+
+            //Values to calculate updates
+            boolean ignoreBroadcast = option;
+
+            //Update
+            jsonObject.put("name", player.getName());
+            jsonObject.put("lastLogIn", lastLoginRETAIN);
+            jsonObject.put("lastLogOut", lastLogoutRETAIN);
+            jsonObject.put("playTime", totalPlayTimeRETAIN);
+            jsonObject.put("firstJoin", firstJoinRETAIN);
+            jsonObject.put("ignoreBroadcast", ignoreBroadcast);
+
+            //Write/Close
+            FileWriter writer = new FileWriter(new File(plugin.getDataFolder().getAbsolutePath() + "/player-logs", player.getName().toLowerCase() + ".json"));
+            writer.write(jsonObject.toJSONString());
+            writer.close();
+//            Bukkit.getServer().getLogger().info("[OSM-Ess] Saved lastLogIn data for " + player.getName() + "! (Filename: " + player.getName().toLowerCase() + ".yml)");
+
+        } catch (IOException ex) {
+            Bukkit.getServer().getLogger().info("[OSM-Ess] Error saving lastLogIn data for " + player.getName() + ": " + ex.getMessage());
+            ex.printStackTrace(System.err);
+        }
+    }
+
+    public boolean hasIgnoreBroadcast(OfflinePlayer player) {
+        if (hasData(player)) {
+            try (FileReader reader = new FileReader(new File(plugin.getDataFolder().getAbsolutePath() + "/player-logs", player.getName().toLowerCase() + ".json"))) {
+                OSMPLUserData data = OSMPLUserData.gson.fromJson(reader, OSMPLUserData.class);
+                if (data.ignoreBroadcast == true) return true;
+                else return false;
+
+            } catch (Exception ex) {
+                ex.printStackTrace(System.err);
+            }
+        }
+        return false;
+    }
 }
