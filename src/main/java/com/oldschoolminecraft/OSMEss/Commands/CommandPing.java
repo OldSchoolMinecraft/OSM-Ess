70f73015ad2145c26b23a67158f06859b5dfa189
SavageUser <LimitlessPowerHD@gmail.com>
2025-10-30 19:38:47 -0400

    Fixed ping returning as 0 when runned, CONSOLE can now use ping on players.

--- src/main/java/com/oldschoolminecraft/OSMEss/Commands/CommandPing.java
+++ src/main/java/com/oldschoolminecraft/OSMEss/Commands/CommandPing.java
@@ -13,13 +13,13 @@ import java.util.HashMap;
 public class CommandPing implements CommandExecutor {
 
     public OSMEss plugin;
-    private final HashMap<String, Long> pingCooldown = new HashMap<>();
+    private final HashMap<String, Long> pingCooldown = new HashMap<>(); // 20 second cooldown to prevent spam.
 
     public CommandPing(OSMEss plugin) {
         this.plugin = plugin;
         this.plugin.getCommand("ping").setExecutor(this);
     }
-  
+
     @Override
     public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
 
@@ -27,33 +27,64 @@ public class CommandPing implements CommandExecutor {
             if (sender instanceof Player) {
                 Player player = (Player) sender;
 
-                if (!pingCooldown.containsKey(player.getName()) || System.currentTimeMillis()/1000 - pingCooldown.get(player.getName())/1000 > 20) {
+                if (args.length == 0 || args.length != 1) {
+                    if (!pingCooldown.containsKey(player.getName()) || System.currentTimeMillis()/1000 - pingCooldown.get(player.getName())/1000 > 25) {
+                        pingCooldown.put(player.getName(), System.currentTimeMillis());
+                        player.sendMessage("§7Your ping is §8" + getPing(player) + " §7ms!");
+                        return true;
+                    }
+                    else {
+                        long cooldownSeconds = System.currentTimeMillis()/1000 - pingCooldown.get(player.getName())/1000;
+                        if (cooldownSeconds == 24) {
+                            player.sendMessage("§cPlease wait 1 second before running /ping again!");
+                            return true;
+                        }
+                        player.sendMessage("§cPlease wait " + (25 - cooldownSeconds) + " seconds before running /ping again!");
+                        return true;
+                    }
+                }
+                Player other = Bukkit.getServer().getPlayer(args[0]);
+
+                if (other == null) {
+                    player.sendMessage("§cPlayer is not online!");
+                    return true;
+                }
+                if (!pingCooldown.containsKey(player.getName()) || System.currentTimeMillis()/1000 - pingCooldown.get(player.getName())/1000 > 25) {
                     pingCooldown.put(player.getName(), System.currentTimeMillis());
-                    player.sendMessage("§7Your ping is §8" + getPing() + " §7ms!");
+                    player.sendMessage("§8" + other.getName() + "§7's ping is §8" + getPing(other) + " §7ms!");
                     return true;
                 }
                 else {
                     long cooldownSeconds = System.currentTimeMillis()/1000 - pingCooldown.get(player.getName())/1000;
-                    if (cooldownSeconds == 19) {
+                    if (cooldownSeconds == 24) {
                         player.sendMessage("§cPlease wait 1 second before running /ping again!");
                         return true;
                     }
-                    player.sendMessage("§cPlease wait " + (20 - cooldownSeconds) + " seconds before running /ping again!");
+                    player.sendMessage("§cPlease wait " + (25 - cooldownSeconds) + " seconds before running /ping again!");
+                    return true;
+                }
+            }
+            else {
+                if (args.length != 1) {
+                    sender.sendMessage("§cUsage: /ping <player>");
+                    return true;
+                }
+                Player other = Bukkit.getServer().getPlayer(args[0]);
+
+                if (other == null) {
+                    sender.sendMessage("§cPlayer is not online!");
                     return true;
                 }
 
-            }
-            else {
-                sender.sendMessage("§cCommand can only be executed by a player!");
+                sender.sendMessage("§8" + other.getName() + "§7's ping is §8" + getPing(other) + " §7ms!");
                 return true;
             }
         }
         return true;
     }
 
-    public int getPing() { //
-//        String address = "os-mc.net"; // Always will ping to OSM when a player runs /ping.
-        String address = Bukkit.getServer().getIp(); // Will ping to your own server's ip when a player runs /ping.
+    public int getPing(Player player) { //
+        String address = player.getAddress().getAddress().getHostAddress(); // Will ping to a specified player's ip from the server address.
         return Math.toIntExact(reachAddressTime(address, 1000));
     }
 
