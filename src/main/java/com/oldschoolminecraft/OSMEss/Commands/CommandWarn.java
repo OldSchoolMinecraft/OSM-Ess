a273896373234b385e590ce9dc58e7d5e811c579
SavageUser <LimitlessPowerHD@gmail.com>
2025-11-10 20:08:38 -0500

    Create CommandWarn.java

---
+++ src/main/java/com/oldschoolminecraft/OSMEss/Commands/CommandWarn.java
@@ -0,0 +1,84 @@
+package com.oldschoolminecraft.OSMEss.Commands;
+
+import com.oldschoolminecraft.OSMEss.OSMEss;
+import org.bukkit.Bukkit;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+
+public class CommandWarn implements CommandExecutor {
+
+    private final OSMEss plugin;
+
+    public CommandWarn(OSMEss plugin) {
+        this.plugin = plugin;
+        this.plugin.getCommand("warn").setExecutor(this);
+    }
+
+    @Override
+    public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
+        if (cmd.getName().equalsIgnoreCase("warn")) {
+
+            if (sender instanceof Player) {
+                Player player = (Player) sender;
+
+                if (player.isOp() || player.hasPermission("osmess.warn")) {
+                    if (args.length < 2) {
+                        player.sendMessage("§cUsage: /warn <player> <reason>");
+                        return true;
+                    }
+
+                    Player other = Bukkit.getServer().getPlayer(args[0].toLowerCase());
+
+                    if (other == null) {
+                        player.sendMessage("§cPlayer is not online!");
+                        return true;
+                    }
+
+                    String reason = "";
+                    for (int i = 1; i < args.length; i++) {
+                        reason = reason + args[i] + " ";
+
+                    }
+
+                    plugin.addWarning(other, reason.trim());
+                    Bukkit.broadcastMessage("§c" + other.getName() + " warned by " + player.getName() + " for:");
+                    Bukkit.broadcastMessage("§4'§e" + reason.replaceAll("\\s+", " ").trim() + "§4'");
+
+                    Bukkit.getLogger().info(other.getName() + " (" + other.getAddress().getAddress().getHostAddress() + ") warned by " + player.getName() + " for: '" + reason.replaceAll("\\s+", " ").trim() + "'");
+                    return true;
+                }
+                else {
+                    player.sendMessage("§cI'm sorry, Dave. I'm afraid I can't do that.");
+                    return true;
+                }
+            }
+            if (args.length < 2) {
+                sender.sendMessage("§cUsage: /warn <player> <reason>");
+                return true;
+            }
+
+            Player other = Bukkit.getServer().getPlayer(args[0]);
+
+            if (other == null) {
+                sender.sendMessage("§cPlayer is not online!");
+                return true;
+            }
+
+            String reason = "";
+            for (int i = 1; i < args.length; i++) {
+                reason = reason + args[i] + " ";
+            }
+
+            plugin.addWarning(other, reason.trim());
+            Bukkit.broadcastMessage("§c" + other.getName() + " warned by CONSOLE for:");
+            Bukkit.broadcastMessage("§4'§e" + reason.replaceAll("\\s+", " ").trim() + "§4'");
+
+            Bukkit.getLogger().info(other.getName() + " (" + other.getAddress().getAddress().getHostAddress() + ") has warned by CONSOLE for: '" + reason.replaceAll("\\s+", " ").trim() + "'");
+            return true;
+        }
+
+        return true;
+    }
+}
