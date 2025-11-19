1a8a0f53516abef50520aec646fd4ca99c42911d
SavageUser <LimitlessPowerHD@gmail.com>
2025-11-18 22:31:24 -0500

    Update PlayerConnectionListener.java

--- src/main/java/com/oldschoolminecraft/OSMEss/Listeners/PlayerConnectionListener.java
+++ src/main/java/com/oldschoolminecraft/OSMEss/Listeners/PlayerConnectionListener.java
@@ -13,7 +13,6 @@ import org.bukkit.event.player.PlayerQuitEvent;
 public class PlayerConnectionListener extends PlayerListener {
 
     public OSMEss plugin;
-
     public PlayerConnectionListener(OSMEss plugin) {
         this.plugin = plugin;
     }
@@ -35,6 +34,16 @@ public class PlayerConnectionListener extends PlayerListener {
             plugin.playtimeHandler.updateLastLogin(player);
             Bukkit.getServer().getLogger().info("[OSM-Ess] Retrieved data for " + player.getName() + "! (Filename: " + player.getName().toLowerCase() + ".json)");
         }
+
+        if (plugin.auctionHandler.hasHostItemsToReturn(player)) {
+            plugin.auctionHandler.returnAuctionHostItems(player);
+            player.sendMessage("§9Items you put for auction have been §breturned§9!");
+        }
+
+        if (plugin.auctionHandler.hasAuctionWonItemsToGive(player)) {
+            plugin.auctionHandler.giveAuctionWonItems(player);
+            player.sendMessage("§9Items you won from an auction have been §bgiven§9!");
+        }
     }
 
     @EventHandler
@@ -42,7 +51,7 @@ public class PlayerConnectionListener extends PlayerListener {
         Player player = event.getPlayer();
         plugin.playtimeHandler.updateTotalPlaytime(player);
 
-        if (Invisiman.instance.isVanished(player)) { //Remove them from arraylist if they quit while vanished.
+        if (Invisiman.instance.isVanished(player)) { // Remove them from arraylist if they quit while vanished.
             if (CommandList.vanished.contains(player)) {
                 CommandList.vanished.remove(player);
             }
