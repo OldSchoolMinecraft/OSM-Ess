582b14aa747dd55032c03a91157265267dcd32dc
SavageUser <LimitlessPowerHD@gmail.com>
2025-11-18 22:33:37 -0500

    Add files via upload

---
+++ src/main/java/com/oldschoolminecraft/OSMEss/Commands/CommandAuction.java
@@ -0,0 +1,88 @@
+package com.oldschoolminecraft.OSMEss.Commands;
+
+import com.oldschoolminecraft.OSMEss.AuctionStatus;
+import com.oldschoolminecraft.OSMEss.OSMEss;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+
+public class CommandAuction implements CommandExecutor {
+
+    private final OSMEss plugin;
+
+    public CommandAuction(OSMEss plugin) {
+        this.plugin = plugin;
+        this.plugin.getCommand("auction").setExecutor(this);
+    }
+
+    @Override
+    public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
+        if (cmd.getName().equalsIgnoreCase("auction")) {
+            if (sender instanceof Player) {
+                Player player = (Player) sender;
+
+                if (args.length == 0) {
+                    if (plugin.auctionHandler.getAuctionStatus() == AuctionStatus.ACTIVE) {
+                        player.sendMessage("§5-= §dAUCTION §5=-");
+                        player.sendMessage("§6Time Left: §e" + plugin.auctionHandler.formatTime(plugin.auctionHandler.getAuctionTimeLeft()));
+                        player.sendMessage("§6Total Bidders: §e" + plugin.auctionHandler.totalBidders);
+                        player.sendMessage("§6Current Bid: §a$" + plugin.auctionHandler.getTopBidAmount());
+                        player.sendMessage("§6Prize: §b" + plugin.auctionHandler.getAuctionItem().getAmount() + "x " +  plugin.auctionHandler.getAuctionItem().getType().name());
+                        return true;
+                    }
+                    else {
+                        if (plugin.isScheduledDeathEnabled()) {
+                            if (plugin.scheduledDeath.getTimeToLive() <= 180) { // Disallow the command within 3 minutes of a restart.
+                                sender.sendMessage("§cCommand is disabled as the server is about to restart!");
+                                return true;
+                            }
+                        }
+
+                        player.sendMessage("§cUsage: /auction <price>");
+                        return true;
+                    }
+                }
+
+                if (args.length != 1) {
+                    player.sendMessage("§cUsage: /auction <price>");
+                    return true;
+                }
+
+                //Todo: Check if auction is running before starting a new one.
+                if (plugin.auctionHandler.getAuctionStatus() == AuctionStatus.ACTIVE) {
+                    player.sendMessage("§cThere is currently an active auction!");
+                    return true;
+                }
+                else {
+                    if (plugin.isScheduledDeathEnabled()) {
+                        if (plugin.scheduledDeath.getTimeToLive() <= 180) { // Disallow the command within 3 minutes of a restart.
+                            sender.sendMessage("§cCommand is disabled as the server is about to restart!");
+                        }
+                    }
+
+                    try {
+                        int price = Integer.parseInt(args[0]);
+
+                        if (price == 0) {
+                            player.sendMessage("§cYou may not start an auction at $0!");
+                            return true;
+                        }
+
+                        plugin.auctionHandler.startAuction(player, price);
+                    } catch (NumberFormatException ex) {
+                        player.sendMessage("§cInvalid integer provided!");
+                    }
+
+                    return true;
+                }
+            }
+            else {
+                sender.sendMessage("§cCommand can only be executed by a player!");
+                return true;
+            }
+        }
+
+        return true;
+    }
+}
---
+++ src/main/java/com/oldschoolminecraft/OSMEss/Commands/CommandBid.java
@@ -0,0 +1,77 @@
+package com.oldschoolminecraft.OSMEss.Commands;
+
+import com.oldschoolminecraft.OSMEss.AuctionStatus;
+import com.oldschoolminecraft.OSMEss.OSMEss;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+
+public class CommandBid implements CommandExecutor {
+
+    private final OSMEss plugin;
+
+    public CommandBid(OSMEss plugin) {
+        this.plugin = plugin;
+        this.plugin.getCommand("bid").setExecutor(this);
+    }
+
+    @Override
+    public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
+        if (cmd.getName().equalsIgnoreCase("bid")) {
+            if (sender instanceof Player) {
+                Player player = (Player) sender;
+
+                if (args.length != 1) {
+                    player.sendMessage("§cUsage: /bid <amount>");
+                    return true;
+                }
+
+                //Todo: Check if auction is running & if player has enough $ to enter the bid.
+                if (plugin.auctionHandler.getAuctionStatus() == AuctionStatus.INACTIVE) {
+                    player.sendMessage("§cThere is no active auction to bid on!");
+                    return true;
+                }
+                else {
+                    if (plugin.auctionHandler.getAuctionHost().getName().equalsIgnoreCase(player.getName())) {
+                        player.sendMessage("§cYou cannot bid on your own auction!");
+                        return true;
+                    }
+                    else {
+                        try {
+                            int amount = Integer.parseInt(args[0]);
+
+                            if (amount < plugin.auctionHandler.getStartingBid()) {
+                                player.sendMessage("§cYour bid amount is lower than the starting bid!");
+                                return true;
+                            }
+
+                            if (plugin.auctionHandler.getTopBidAmount() >= amount) {
+                                player.sendMessage("§cYour bid amount must be higher than $" + plugin.auctionHandler.getTopBidAmount() + "!");
+                                return true;
+                            }
+                            if (amount >= plugin.essentials.getUser(player).getMoney()) {
+                                player.sendMessage("§cYou don't have enough money to bid this amount!");
+                                return true;
+                            }
+                            else {
+                                plugin.auctionHandler.addToAuction(player, amount);
+                            }
+
+                        } catch (NumberFormatException ex) {
+                            player.sendMessage("§cInvalid integer provided!");
+                        }
+
+                        return true;
+                    }
+                }
+            }
+            else {
+                sender.sendMessage("§cCommand can only be executed by a player!");
+                return true;
+            }
+        }
+
+        return true;
+    }
+}
