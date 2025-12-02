0471e459039b3d19be6f56550f6baa15e7612a57
SavageUser <LimitlessPowerHD@gmail.com>
2025-12-01 22:46:15 -0500

    Update PlayerWorldListener.java

--- src/main/java/com/oldschoolminecraft/OSMEss/Listeners/PlayerWorldListener.java
+++ src/main/java/com/oldschoolminecraft/OSMEss/Listeners/PlayerWorldListener.java
@@ -29,7 +29,63 @@ public class PlayerWorldListener implements Listener {
         if (player.isOp() || player.hasPermission("osmess.chatcolor")) {
             if (!plugin.hasChatColorMessageSet(player)) return;
 
-            event.setMessage(plugin.getChatColorMessageSetting(player) + event.getMessage());
+//            event.setMessage(plugin.getChatColorMessageSetting(player) + event.getMessage());
+
+            if (plugin.getChatColorMessageSetting(player).equals("&0")) {
+                event.setMessage(ChatColor.BLACK + event.getMessage());
+            }
+            if (plugin.getChatColorMessageSetting(player).equals("&1")) {
+                event.setMessage(ChatColor.DARK_BLUE + event.getMessage());
+            }
+            if (plugin.getChatColorMessageSetting(player).equals("&2")) {
+                event.setMessage(ChatColor.DARK_GREEN + event.getMessage());
+            }
+            if (plugin.getChatColorMessageSetting(player).equals("&3")) {
+                event.setMessage(ChatColor.DARK_AQUA + event.getMessage());
+            }
+            if (plugin.getChatColorMessageSetting(player).equals("&4")) {
+                event.setMessage(ChatColor.DARK_RED + event.getMessage());
+            }
+            if (plugin.getChatColorMessageSetting(player).equals("&5")) {
+                event.setMessage(ChatColor.DARK_PURPLE + event.getMessage());
+            }
+            if (plugin.getChatColorMessageSetting(player).equals("&6")) {
+                event.setMessage(ChatColor.GOLD + event.getMessage());
+            }
+            if (plugin.getChatColorMessageSetting(player).equals("&7")) {
+                event.setMessage(ChatColor.GRAY + event.getMessage());
+            }
+            if (plugin.getChatColorMessageSetting(player).equals("&8")) {
+                event.setMessage(ChatColor.DARK_GRAY + event.getMessage());
+            }
+            if (plugin.getChatColorMessageSetting(player).equals("&9")) {
+                event.setMessage(ChatColor.BLUE + event.getMessage());
+            }
+            if (plugin.getChatColorMessageSetting(player).equals("&a")) {
+                event.setMessage(ChatColor.GREEN + event.getMessage());
+            }
+            if (plugin.getChatColorMessageSetting(player).equals("&b")) {
+                event.setMessage(ChatColor.AQUA + event.getMessage());
+            }
+            if (plugin.getChatColorMessageSetting(player).equals("&c")) {
+                event.setMessage(ChatColor.RED + event.getMessage());
+            }
+            if (plugin.getChatColorMessageSetting(player).equals("&d")) {
+                event.setMessage(ChatColor.LIGHT_PURPLE + event.getMessage());
+            }
+            if (plugin.getChatColorMessageSetting(player).equals("&e")) {
+                event.setMessage(ChatColor.YELLOW + event.getMessage());
+            }
+            if (plugin.getChatColorMessageSetting(player).equals("&f")) {
+                event.setMessage(ChatColor.WHITE + event.getMessage());
+            }
+            else {
+                event.setMessage(event.getMessage());
+//                event.setMessage(ChatColor.WHITE + event.getMessage());
+            }
+        }
+        else {
+            event.setMessage(event.getMessage());
         }
     }
 
@@ -120,4 +176,5 @@ public class PlayerWorldListener implements Listener {
             }
         }
     }
+
 }
