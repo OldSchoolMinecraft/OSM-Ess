4f3934263462f642371363e002753e8476d0de75
SavageUser <LimitlessPowerHD@gmail.com>
2026-01-14 14:40:38 -0500

    allow CONSOLE to use /homes.

--- src/main/java/com/oldschoolminecraft/OSMEss/Commands/CommandHomes.java
+++ src/main/java/com/oldschoolminecraft/OSMEss/Commands/CommandHomes.java
@@ -32,6 +32,7 @@ public class CommandHomes implements CommandExecutor {
                         Player other = Bukkit.getServer().getPlayer(args[1]);
 
                         if (other == null) {
+                            // Paginate the offline player's homes to the player command sender.
                             OfflinePlayer offline = Bukkit.getServer().getOfflinePlayer(args[1]);
 
                             if (plugin.essentials.getOfflineUser(offline.getName()) == null) {
@@ -55,7 +56,7 @@ public class CommandHomes implements CommandExecutor {
                                     int linesPerPage = 5;
                                     int totalPages = (int)Math.ceil((double)homes.size() / (double)linesPerPage);
                                     int startingRecord = page * linesPerPage;
-                                    int titlePageNum = page+1;
+                                    int titlePageNum = page + 1;
 
                                     if (page < 0 || page > totalPages - 1) {
                                         player.sendMessage(plugin.invalidPageNum);
@@ -65,7 +66,7 @@ public class CommandHomes implements CommandExecutor {
                                     player.sendMessage("§8" + offline.getName().toLowerCase() + "§7's Homes (§3" + homes.size() + "§7) §7Page §8" + titlePageNum + " §7of §8" + totalPages + "§7:");
 
                                     for (int i = startingRecord; i < homes.size() && i < startingRecord + linesPerPage; i++) {
-                                        player.sendMessage("§7- §8" + homes.get(i) + " §7(§8/home " + homes.get(i) + "§7)");
+                                        player.sendMessage("§7- §8" + homes.get(i) + " §7(§b/home §3" + offline.getName().toLowerCase() + ":" + homes.get(i) + "§7)");
                                     }
 
                                     return true;
@@ -82,6 +83,7 @@ public class CommandHomes implements CommandExecutor {
                             return true;
                         }
 
+                        // Paginate the online player's homes to the player command sender.
                         int page;
 
                         try {
@@ -98,7 +100,7 @@ public class CommandHomes implements CommandExecutor {
                                 int linesPerPage = 5;
                                 int totalPages = (int)Math.ceil((double)homes.size() / (double)linesPerPage);
                                 int startingRecord = page * linesPerPage;
-                                int titlePageNum = page+1;
+                                int titlePageNum = page + 1;
 
                                 if (page < 0 || page > totalPages - 1) {
                                     player.sendMessage(plugin.invalidPageNum);
@@ -108,7 +110,7 @@ public class CommandHomes implements CommandExecutor {
                                 player.sendMessage("§8" + other.getName() + "§7's Homes (§3" + homes.size() + "§7) §7Page §8" + titlePageNum + " §7of §8" + totalPages + "§7:");
 
                                 for (int i = startingRecord; i < homes.size() && i < startingRecord + linesPerPage; i++) {
-                                    player.sendMessage("§7- §8" + homes.get(i) + " §7(§8/home " + homes.get(i) + "§7)");
+                                    player.sendMessage("§7- §8" + homes.get(i) + " §7(§b/home §3" + other.getName() + ":" + homes.get(i) + "§7)");
                                 }
 
                                 return true;
@@ -131,6 +133,7 @@ public class CommandHomes implements CommandExecutor {
                 }
 
                 else if (args.length == 0 || args.length == 1) {
+                    // Paginate the player's own homes.
                     int page;
 
                     try {
@@ -147,7 +150,7 @@ public class CommandHomes implements CommandExecutor {
                             int linesPerPage = 5;
                             int totalPages = (int)Math.ceil((double)homes.size() / (double)linesPerPage);
                             int startingRecord = page * linesPerPage;
-                            int titlePageNum = page+1;
+                            int titlePageNum = page + 1;
 
                             if (page < 0 || page > totalPages - 1) {
                                 player.sendMessage(plugin.invalidPageNum);
@@ -157,7 +160,7 @@ public class CommandHomes implements CommandExecutor {
                             player.sendMessage("§7Page §8" + titlePageNum + " §7of §8" + totalPages + "§7.");
 
                             for (int i = startingRecord; i < homes.size() && i < startingRecord + linesPerPage; i++) {
-                                player.sendMessage("§7- §8" + homes.get(i) + " §7(§8/home " + homes.get(i) + "§7)");
+                                player.sendMessage("§7- §8" + homes.get(i) + " §7(§b/home §3" + homes.get(i) + "§7)");
                             }
 
                             return true;
@@ -185,7 +188,106 @@ public class CommandHomes implements CommandExecutor {
                 }
             }
             else {
-                sender.sendMessage("Command can only be executed by a player!");
+                if (args.length != 2) {
+                    sender.sendMessage("Usage: /homes <page> <player>");
+                    return true;
+                }
+
+                Player other = Bukkit.getServer().getPlayer(args[1]);
+
+                if (other == null) {
+                    OfflinePlayer offline = Bukkit.getServer().getOfflinePlayer(args[1]);
+
+                    if (plugin.essentials.getOfflineUser(offline.getName()) == null) {
+                        sender.sendMessage("Error: Player never logged in before. (no Essentials data)");
+                        return true;
+                    }
+
+                    // Paginate the offline player's homes to the CONSOLE.
+                    int page;
+
+                    try {
+                        page = args.length == 0 ? 0 : Integer.valueOf(args[0]) - 1;
+
+                        if (plugin.essentials.getUser(offline.getName().toLowerCase()).hasHome() || !plugin.essentials.getUser(offline.getName().toLowerCase()).getHomes().isEmpty()) {
+                            List<String> homes = plugin.essentials.getUser(offline.getName().toLowerCase()).getHomes();
+
+                            if (homes.isEmpty()) {
+                                sender.sendMessage("Error: Player doesn't have any homes.");
+                                return true;
+                            }
+
+                            int linesPerPage = 5;
+                            int totalPages = (int)Math.ceil((double)homes.size() / (double)linesPerPage);
+                            int startingRecord = page * linesPerPage;
+                            int titlePageNum = page + 1;
+
+                            if (page < 0 || page > totalPages - 1) {
+                                sender.sendMessage("Error: Invalid page number provided.");
+                                return true;
+                            }
+
+                            sender.sendMessage(offline.getName().toLowerCase() + "'s Homes (" + homes.size() + ") Page " + titlePageNum + " of " + totalPages + ":");
+
+                            for (int i = startingRecord; i < homes.size() && i < startingRecord + linesPerPage; i++) {
+                                sender.sendMessage("- " + homes.get(i));
+                            }
+
+                            return true;
+                        }
+                        else {
+                            sender.sendMessage("Error: Player doesn't have any homes.");
+                            return true;
+                        }
+
+                    } catch (NumberFormatException ex) {
+                        sender.sendMessage("Error: Invalid integer provided.");
+                    }
+
+                    return true;
+                }
+
+                // Paginate the online player's homes to the CONSOLE.
+                int page;
+
+                try {
+                    page = args.length == 0 ? 0 : Integer.valueOf(args[0]) - 1;
+
+                    if (plugin.essentials.getUser(other).hasHome() || !plugin.essentials.getUser(other).getHomes().isEmpty()) {
+                        List<String> homes = plugin.essentials.getUser(other).getHomes();
+
+                        if (homes.isEmpty()) {
+                            sender.sendMessage("Error: Player doesn't have any homes.");
+                            return true;
+                        }
+
+                        int linesPerPage = 5;
+                        int totalPages = (int)Math.ceil((double)homes.size() / (double)linesPerPage);
+                        int startingRecord = page * linesPerPage;
+                        int titlePageNum = page + 1;
+
+                        if (page < 0 || page > totalPages - 1) {
+                            sender.sendMessage("Error: Invalid page number provided.");
+                            return true;
+                        }
+
+                        sender.sendMessage(other.getName() + "'s Homes (" + homes.size() + ") Page " + titlePageNum + " of " + totalPages + ":");
+
+                        for (int i = startingRecord; i < homes.size() && i < startingRecord + linesPerPage; i++) {
+                            sender.sendMessage("- " + homes.get(i));
+                        }
+
+                        return true;
+                    }
+                    else {
+                        sender.sendMessage("Error: Player doesn't have any homes.");
+                        return true;
+                    }
+
+                } catch (NumberFormatException ex) {
+                    sender.sendMessage("Error: Invalid integer provided.");
+                }
+
                 return true;
             }
         }
