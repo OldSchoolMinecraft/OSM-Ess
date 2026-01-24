e95d005afeee99854e67c7d9fecf7aaede01e9eb
SavageUser <LimitlessPowerHD@gmail.com>
2026-01-24 02:58:50 -0500

    Update CommandOSMEss.java; Overhaul.

--- src/main/java/com/oldschoolminecraft/OSMEss/Commands/CommandOSMEss.java
+++ src/main/java/com/oldschoolminecraft/OSMEss/Commands/CommandOSMEss.java
@@ -3,10 +3,14 @@ package com.oldschoolminecraft.OSMEss.Commands;
 import com.oldschoolminecraft.OSMEss.AuctionStatus;
 import com.oldschoolminecraft.OSMEss.OSMEss;
 import org.bukkit.Bukkit;
+import org.bukkit.OfflinePlayer;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class CommandOSMEss implements CommandExecutor {
 
@@ -25,12 +29,47 @@ public class CommandOSMEss implements CommandExecutor {
 
                 if (player.isOp() || player.hasPermission("osmess.command")) {
                     if (args.length != 1) {
-                        player.sendMessage("§7OSM-Ess version " + plugin.getDescription().getVersion() + "!");
-                        player.sendMessage("§7Administration Commands:");
-                        player.sendMessage("§b/osmess §3endauction §8- §7Ends a current auction.");
-                        player.sendMessage("§b/osmess §3reload §8- §7Reloads all yml files.");
-                        player.sendMessage("§b/osmess §3toggleauction §8- §7Enables/Disables the auction system.");
+                        player.sendMessage("§7OSM-Ess version §2" + plugin.getDescription().getVersion());
+                        if (player.isOp()) {player.sendMessage("§7Administration Commands:");} // Show 'Administration' if they're opped.
+                        else {player.sendMessage("§7Available Commands:");}
+
+                        // Show command usages that staff (Mods, Sr. Mods) have permission to, assuming they have the 'osmess.command' permission.
+                        if (player.isOp() || player.hasPermission("osmess.command.eablacklist")) {player.sendMessage("§b/osmess §3eablacklist - §7Shows a list of players blocked from /ea.");}
+                        if (player.isOp() || player.hasPermission("osmess.command.endauction")) {player.sendMessage("§b/osmess §3endauction §8- §7Ends a current auction.");}
+                        if (player.isOp() || player.hasPermission("osmess.command.reload")) {player.sendMessage("§b/osmess §3reload §8- §7Reloads all yml files.");}
+                        if (player.isOp() || player.hasPermission("osmess.command.toggleauction")) {player.sendMessage("§b/osmess §3toggleauction §8- §7Enables/Disables the auction system.");}
+                        if (player.isOp() || player.hasPermission("osmess.command.toggleea")) {player.sendMessage("§b/osmess §3toggleea §8- §7Enables/Disables explosive arrows.");}
                         return true;
+                    }
+
+                    if (args[0].equalsIgnoreCase("eablacklist")) {
+
+                        if (player.isOp() || player.hasPermission("osmess.command.eablacklist")) {
+                            List<String> eaBlacklist = plugin.configSettingCFG.getStringList("ExplosiveArrows.disallowedPlayers", new ArrayList<>());
+
+                            if (eaBlacklist.isEmpty()) {
+                                player.sendMessage("§7Blacklisted Players (§40§7): §8None");
+                                return true;
+                            }
+                            else {
+                                StringBuilder stringBuilder = new StringBuilder();
+
+                                for (String blacklisted : eaBlacklist) {
+                                    if (stringBuilder.length() > 0) {
+                                        stringBuilder.append(", ");
+                                    }
+
+                                    stringBuilder.append("§8" + blacklisted + "§7");
+                                }
+
+                                player.sendMessage("§7Blacklisted Players (§3" + eaBlacklist.size() + "§7): §8" + stringBuilder);
+                                return true;
+                            }
+                        }
+                        else {
+                            player.sendMessage(plugin.noPermission);
+                            return true;
+                        }
                     }
 
                     if (args[0].equalsIgnoreCase("endauction")) {
@@ -94,32 +133,185 @@ public class CommandOSMEss implements CommandExecutor {
                             return true;
                         }
                     }
+                    if (args[0].equalsIgnoreCase("toggleea")) {
+                        if (player.isOp() || player.hasPermission("osmess.command.toggleea")) {
+
+                            if (plugin.isExplosiveArrowsEnabled()) {
+                                plugin.setExplodingArrows(false);
+
+                                if (!CommandExplosiveArrows.explodeArrow.isEmpty()) {
+                                    CommandExplosiveArrows.explodeArrow.clear();
+                                }
+
+                                player.sendMessage("§fExploding Arrows are now §4disabled§7!");
+                                return true;
+                            }
+                            else {
+                                plugin.setExplodingArrows(true);
+
+                                player.sendMessage("§fExploding Arrows are now §aenabled§7!");
+                                return true;
+                            }
+                        }
+                        else {
+                            player.sendMessage(plugin.noPermission);
+                            return true;
+                        }
+                    }
+
                     else {
-                        player.sendMessage("§7OSM-Ess version " + plugin.getDescription().getVersion() + "!");
-                        player.sendMessage("§7Administration Commands:");
-                        player.sendMessage("§b/osmess §3endauction §8- §7Ends a current auction.");
-                        player.sendMessage("§b/osmess §3reload §8- §7Reloads all yml files.");
-                        player.sendMessage("§b/osmess §3toggleauction §8- §7Enables/Disables the auction system.");
+                        player.sendMessage("§7OSM-Ess version §2" + plugin.getDescription().getVersion());
+                        if (player.isOp()) {player.sendMessage("§7Administration Commands:");} // Show 'Administration' if they're opped.
+                        else {player.sendMessage("§7Available Commands:");}
+
+                        // Show command usages that staff (Mods, Sr. Mods) have permission to, assuming they have the 'osmess.command' permission.
+                        if (player.isOp() || player.hasPermission("osmess.command.eablacklist")) {player.sendMessage("§b/osmess §3eablacklist - §7Shows a list of players blocked from /ea.");}
+                        if (player.isOp() || player.hasPermission("osmess.command.endauction")) {player.sendMessage("§b/osmess §3endauction §8- §7Ends a current auction.");}
+                        if (player.isOp() || player.hasPermission("osmess.command.reload")) {player.sendMessage("§b/osmess §3reload §8- §7Reloads all yml files.");}
+                        if (player.isOp() || player.hasPermission("osmess.command.toggleauction")) {player.sendMessage("§b/osmess §3toggleauction §8- §7Enables/Disables the auction system.");}
+                        if (player.isOp() || player.hasPermission("osmess.command.toggleea")) {player.sendMessage("§b/osmess §3toggleea §8- §7Enables/Disables exploding arrows.");}
                         return true;
                     }
                 }
                 else {
-                    player.sendMessage("§7OSM-Ess version " + plugin.getDescription().getVersion() + "!");
+                    player.sendMessage("§7OSM-Ess version " + plugin.getDescription().getVersion());
                     return true;
                 }
             }
             else { // CONSOLE executed the command.
-                if (args.length != 1) {
-                    sender.sendMessage("OSM-Ess version " + plugin.getDescription().getVersion() + "!");
+                if (args.length == 0) {
+                    sender.sendMessage("OSM-Ess version " + plugin.getDescription().getVersion());
                     sender.sendMessage("Administration Commands:");
+                    sender.sendMessage("/osmess addtoeablacklist - Adds a player to the blacklist for /ea.");
+                    sender.sendMessage("/osmess delfromeablacklist - Removes a player from the blacklist for /ea.");
+                    sender.sendMessage("/osmess eablacklist - Shows a list of players blocked from /ea.");
                     sender.sendMessage("/osmess endauction - Ends a current auction.");
                     sender.sendMessage("/osmess reload - Reloads all yml files.");
-//                    sender.sendMessage("/osmess setplaytime - Modifies a player's playtime data. (CONSOLE only)"); // Coming Soon.
                     sender.sendMessage("/osmess toggleauction - Enables/Disables the auction system.");
+                    sender.sendMessage("/osmess toggleea - Enables/Disables exploding arrows.");
                     return true;
                 }
 
+                if (args[0].equalsIgnoreCase("addtoeablacklist")) {
+                    if (args.length != 2) {
+                        sender.sendMessage("Usage: /osmess addtoeablacklist <player>");
+                        return true;
+                    }
+
+                    Player other = Bukkit.getServer().getPlayer(args[1]);
+
+                    if (other == null) {
+                        OfflinePlayer offline = Bukkit.getServer().getOfflinePlayer(args[1]);
+
+                        if (!plugin.playerDataHandler.hasData(offline)) {
+                            sender.sendMessage("Error: Player never logged in before.");
+                            return true;
+                        }
+
+                        if (plugin.essentials.getOfflineUser(offline.getName()) == null) {
+                            sender.sendMessage("Error: Player never logged in before. (no Essentials data)");
+                            return true;
+                        }
+
+                        if (plugin.isOnEABlacklist(offline)) {
+                            sender.sendMessage("Error: Player is already on the explosive arrow blacklist.");
+                            return true;
+                        }
+                        else {
+                            plugin.addToEABlacklist(offline);
+                            sender.sendMessage("Player " + offline.getName() + " added to explosive arrow blacklist.");
+                            return true;
+                        }
+                    }
+
+                    if (plugin.isOnEABlacklist(other)) {
+                        sender.sendMessage("Error: Player is already on the explosive arrow blacklist.");
+                        return true;
+                    }
+                    else {
+                        plugin.addToEABlacklist(other);
+                        sender.sendMessage(other.getName() + " added to explosive arrow blacklist.");
+                        return true;
+                    }
+                }
+
+                if (args[0].equalsIgnoreCase("delfromeablacklist")) {
+                    if (args.length != 2) {
+                        sender.sendMessage("Usage: /osmess delfromeablacklist <player>");
+                        return true;
+                    }
+
+                    Player other = Bukkit.getServer().getPlayer(args[1]);
+
+                    if (other == null) {
+                        OfflinePlayer offline = Bukkit.getServer().getOfflinePlayer(args[1]);
+
+                        if (!plugin.playerDataHandler.hasData(offline)) {
+                            sender.sendMessage("Error: Player never logged in before.");
+                            return true;
+                        }
+
+                        if (plugin.essentials.getOfflineUser(offline.getName()) == null) {
+                            sender.sendMessage("Error: Player never logged in before. (no Essentials data)");
+                            return true;
+                        }
+
+                        if (!plugin.isOnEABlacklist(offline)) {
+                            sender.sendMessage("Error: Player isn't on the explosive arrow blacklist.");
+                            return true;
+                        }
+                        else {
+                            plugin.delFromEABlacklist(offline);
+                            sender.sendMessage(offline.getName() + " removed from explosive arrow blacklist.");
+                            return true;
+                        }
+                    }
+
+                    if (!plugin.isOnEABlacklist(other)) {
+                        sender.sendMessage("Error: Player isn't on the explosive arrow blacklist.");
+                        return true;
+                    }
+                    else {
+                        plugin.delFromEABlacklist(other);
+                        sender.sendMessage(other.getName() + " removed from explosive arrow blacklist.");
+                        return true;
+                    }
+                }
+
+                if (args[0].equalsIgnoreCase("eablacklist")) {
+                    if (args.length != 1) {
+                        sender.sendMessage("Usage: /osmess eablacklist");
+                        return true;
+                    }
+
+                    List<String> eaBlacklist = plugin.configSettingCFG.getStringList("ExplosiveArrows.disallowedPlayers", new ArrayList<>());
+
+                    if (eaBlacklist.isEmpty()) {
+                        Bukkit.getServer().getLogger().info("[OSM-Ess] Blacklisted Players (" + eaBlacklist.size() + "): None");
+                        return true;
+                    }
+                    else {
+                        StringBuilder stringBuilder = new StringBuilder();
+
+                        for (String blacklisted : eaBlacklist) {
+                            if (stringBuilder.length() > 0) {
+                                stringBuilder.append(", ");
+                            }
+
+                            stringBuilder.append(blacklisted);
+                        }
+
+                        Bukkit.getServer().getLogger().info("[OSM-Ess] Blacklisted Players (" + eaBlacklist.size() + "): " + stringBuilder);
+                        return true;
+                    }
+                }
+
                 if (args[0].equalsIgnoreCase("endauction")) {
+                    if (args.length != 1) {
+                        sender.sendMessage("Usage: /osmess endauction");
+                        return true;
+                    }
+
                     if (plugin.auctionHandler.getAuctionStatus() == AuctionStatus.INACTIVE) {
                         sender.sendMessage("There is no auction to forcefully end!");
                         return true;
@@ -130,7 +322,13 @@ public class CommandOSMEss implements CommandExecutor {
                     Bukkit.getServer().getLogger().info("Auction was forcefully ended!");
                     return true;
                 }
+
                 if (args[0].equalsIgnoreCase("reload") || args[0].equalsIgnoreCase("reloadcfg")) {
+                    if (args.length != 1) {
+                        sender.sendMessage("Usage: /osmess reload");
+                        return true;
+                    }
+
                     plugin.configSettingCFG.reload();
                     plugin.colorMessageCFG.reload();
                     plugin.warningsCFG.reload();
@@ -139,6 +337,11 @@ public class CommandOSMEss implements CommandExecutor {
                     return true;
                 }
                 if (args[0].equalsIgnoreCase("toggleauction") ||  args[0].equalsIgnoreCase("toggleauctionsystem")) {
+                    if (args.length != 1) {
+                        sender.sendMessage("Usage: /osmess toggleauction");
+                        return true;
+                    }
+
                     if (plugin.isAuctionSystemEnabled()) {
                         if (plugin.auctionHandler.getAuctionStatus() == AuctionStatus.ACTIVE) {
                             sender.sendMessage("There is currently an active auction!");
@@ -159,11 +362,38 @@ public class CommandOSMEss implements CommandExecutor {
                         return true;
                     }
                 }
-                sender.sendMessage("OSM-Ess version " + plugin.getDescription().getVersion() + "!");
+
+                if (args[0].equalsIgnoreCase("toggleea")) {
+                    if (args.length != 1) {
+                        sender.sendMessage("Usage: /osmess toggleea");
+                        return true;
+                    }
+
+                    if (plugin.isExplosiveArrowsEnabled()) {
+                        plugin.setExplodingArrows(false);
+
+                        CommandExplosiveArrows.explodeArrow.clear();
+
+                        Bukkit.getServer().getLogger().info("Exploding Arrows are now disabled!");
+                        return true;
+                    }
+                    else {
+                        plugin.setExplodingArrows(true);
+
+                        Bukkit.getServer().getLogger().info("Exploding Arrows are now enabled!");
+                        return true;
+                    }
+                }
+
+                sender.sendMessage("OSM-Ess version " + plugin.getDescription().getVersion());
                 sender.sendMessage("Administration Commands:");
+                sender.sendMessage("/osmess addtoeablacklist - Adds a player to the blacklist for /ea.");
+                sender.sendMessage("/osmess delfromeablacklist - Removes a player from the blacklist for /ea.");
+                sender.sendMessage("/osmess eablacklist - Shows a list of players blocked from /ea.");
                 sender.sendMessage("/osmess endauction - Ends a current auction.");
                 sender.sendMessage("/osmess reload - Reloads all yml files.");
                 sender.sendMessage("/osmess toggleauction - Enables/Disables the auction system.");
+                sender.sendMessage("/osmess toggleea - Enables/Disables exploding arrows.");
                 return true;
             }
         }
