5c677b1fdf1fd2feaa45444ce88b4f0d9ce5371b
SavageUser <LimitlessPowerHD@gmail.com>
2026-02-13 22:19:55 -0500

    Update PlayerDataHandler.java

--- src/main/java/com/oldschoolminecraft/OSMEss/Handlers/PlayerDataHandler.java
+++ src/main/java/com/oldschoolminecraft/OSMEss/Handlers/PlayerDataHandler.java
@@ -12,9 +12,72 @@ import java.io.*;
 public class PlayerDataHandler {
 
     public OSMEss plugin;
+    public static File TIMEZONE_DATA_DIR;
 
     public PlayerDataHandler(OSMEss plugin) {
         this.plugin = plugin;
+        TIMEZONE_DATA_DIR = new File("timezone-settings");
+    }
+
+
+    public void createPlayerTimeZoneSetting(Player player) {
+        try {
+            if (!TIMEZONE_DATA_DIR.exists()) {
+                TIMEZONE_DATA_DIR.mkdirs();
+            }
+
+            File timezoneFile = new File(TIMEZONE_DATA_DIR, player.getName().toLowerCase() + ".json");
+
+            if (!timezoneFile.exists()) {
+                timezoneFile.createNewFile();
+            }
+
+            JSONObject jsonObject = new JSONObject();
+            jsonObject.put("name", player.getName());
+            jsonObject.put("timeZone", "UTC");
+
+            Writer writer = new FileWriter(timezoneFile, false);
+            writer.write(jsonObject.toString());
+            writer.flush();
+            writer.close();
+
+            Bukkit.getServer().getLogger().info("[OSM-Ess] Created & saved timezone data for " + player.getName() + "! (Filename: " + timezoneFile.getName() + ")");
+        } catch (IOException ex) {
+            Bukkit.getServer().getLogger().info("[OSM-Ess] Error creating TimeZone data for " + player.getName() + ": " + ex.getMessage());
+            ex.printStackTrace(System.err);
+        }
+    }
+
+    public void updatePlayerTimeZone(Player player, String timeZone) {
+        try {
+            JSONObject jsonObject = new JSONObject();
+
+            //Values to calculate updates
+
+            //Update
+            jsonObject.put("name", player.getName());
+            jsonObject.put("timeZone", timeZone);
+
+
+            //Write/Close
+            FileWriter writer = new FileWriter(new File(TIMEZONE_DATA_DIR, player.getName().toLowerCase() + ".json"));
+            writer.write(jsonObject.toJSONString());
+            writer.close();
+
+        } catch (IOException ex) {
+            Bukkit.getServer().getLogger().info("[OSM-Ess] Error saving TimeZone data for " + player.getName() + ": " + ex.getMessage());
+            ex.printStackTrace(System.err);
+        }
+
+    }
+
+    public boolean hasTimeZoneData(OfflinePlayer player) {
+        File timezoneFile = new File(TIMEZONE_DATA_DIR, player.getName().toLowerCase() + ".json");
+
+        if (timezoneFile.exists()) {
+            return true;
+        }
+        else return false;
     }
 
 
