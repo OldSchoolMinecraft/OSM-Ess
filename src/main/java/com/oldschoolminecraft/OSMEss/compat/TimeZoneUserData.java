36cd2bbd7e93cd4693ee8679d40b47d1e3dba781
SavageUser <LimitlessPowerHD@gmail.com>
2026-02-13 22:12:51 -0500

    Create TimeZoneUserData.java

---
+++ src/main/java/com/oldschoolminecraft/OSMEss/compat/TimeZoneUserData.java
@@ -0,0 +1,10 @@
+package com.oldschoolminecraft.OSMEss.compat;
+
+import com.google.gson.Gson;
+
+public class TimeZoneUserData {
+
+    public static final Gson gson = new Gson();
+    public String name;
+    public String timeZone;
+}
