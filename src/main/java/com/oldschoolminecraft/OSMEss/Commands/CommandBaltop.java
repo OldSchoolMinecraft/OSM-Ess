cc5f188c9eaec0cef607739009b7ec63a25f3fd9
SavageUser <LimitlessPowerHD@gmail.com>
2026-03-27 20:02:45 -0400

    Update CommandBaltop.java; BusinessBal implementation.

--- src/main/java/com/oldschoolminecraft/OSMEss/Commands/CommandBaltop.java
+++ src/main/java/com/oldschoolminecraft/OSMEss/Commands/CommandBaltop.java
@@ -4,6 +4,7 @@ import com.oldschoolminecraft.OSMEss.OSMEss;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
 
 public class CommandBaltop implements CommandExecutor {
 
@@ -18,31 +19,168 @@ public class CommandBaltop implements CommandExecutor {
     public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
 
         if (cmd.getName().equalsIgnoreCase("baltop")) {
-            if (plugin.isScheduledDeathEnabled()) {
-                if (plugin.scheduledDeath.getTimeToLive() <= 30) {
-                    sender.sendMessage(plugin.cmdDisabledRestart);
-                    return true;
+            if (sender instanceof Player) {
+                Player player = (Player) sender;
+
+                if (plugin.isScheduledDeathEnabled()) {
+                    if (plugin.scheduledDeath.getTimeToLive() <= 30) {
+                        player.sendMessage(plugin.cmdDisabledRestart);
+                        return true;
+                    }
                 }
-            }
 
-            sender.sendMessage("§7Users with the top balances (refreshes hourly):");
-            java.util.List<java.util.Map.Entry<String, Integer>> topBalances = getTopBalances(10);
+                if (args.length == 0) { // Show regular balance top 10 from Essentials.
+                    player.sendMessage("§7Users with the top balances (refreshes hourly):");
+                    java.util.List<java.util.Map.Entry<String, Integer>> topBalances = getTopBalances(10);
 
-            if (topBalances.isEmpty()) {
-                sender.sendMessage("§cNo balance data available yet.");
-                return true;
+                    if (topBalances.isEmpty()) {
+                        player.sendMessage("§cNo balance data available yet.");
+                        return true;
+                    }
+                    else {
+                        int rank = 1; //Ripped from LoginStreaks.
+                        for (java.util.Map.Entry<String, Integer> entry : topBalances) {
+                            String playerName = entry.getKey();
+                            int mostMoney = entry.getValue();
+
+                            player.sendMessage("§8" + rank + "§7. §7" + playerName + ": §8$" + mostMoney);
+                            rank++;
+                        }
+
+                        return true;
+                    }
+                }
+
+                if (args[0].equalsIgnoreCase("businessbal") || args[0].equalsIgnoreCase("bbal")) { // Show BusinessBal top 10.
+                    if (plugin.isBusinessBalEnabled()) {
+                        player.sendMessage("§7BusinessBals with the top balances (refreshes hourly):");
+
+                        java.util.List<java.util.Map.Entry<String, Double>> topBizBalances = getTopBusinessBals(10);
+
+                        if (topBizBalances.isEmpty()) {
+                            player.sendMessage("§cNo balance data available yet.");
+                            return true;
+                        }
+                        else {
+                            int rank = 1;
+                            for (java.util.Map.Entry<String, Double> entry : topBizBalances) {
+                                String accountName = entry.getKey();
+                                double mostMoney = entry.getValue();
+
+                                player.sendMessage("§8" + rank + "§7. §7" + accountName + ": §8$" + mostMoney);
+                                rank++;
+                            }
+
+                            return true;
+                        }
+
+                    }
+                    else {
+                        player.sendMessage("§cBusinessBal is not installed.");
+                        return true;
+                    }
+                }
+                else { // Show regular balance top 10 from Essentials.
+                    player.sendMessage("§7Users with the top balances (refreshes hourly):");
+                    java.util.List<java.util.Map.Entry<String, Integer>> topBalances = getTopBalances(10);
+
+                    if (topBalances.isEmpty()) {
+                        player.sendMessage("§cNo balance data available yet.");
+                        return true;
+                    }
+                    else {
+                        int rank = 1; //Ripped from LoginStreaks.
+                        for (java.util.Map.Entry<String, Integer> entry : topBalances) {
+                            String playerName = entry.getKey();
+                            int mostMoney = entry.getValue();
+
+                            player.sendMessage("§8" + rank + "§7. §7" + playerName + ": §8$" + mostMoney);
+                            rank++;
+                        }
+
+                        return true;
+                    }
+                }
             }
             else {
-                int rank = 1; //Ripped from LoginStreaks.
-                for (java.util.Map.Entry<String, Integer> entry : topBalances) {
-                    String playerName = entry.getKey();
-                    int mostMoney = entry.getValue();
-
-                    sender.sendMessage("§8" + rank + "§7. §7" + playerName + ": §8$" + mostMoney);
-                    rank++;
+                if (plugin.isScheduledDeathEnabled()) {
+                    if (plugin.scheduledDeath.getTimeToLive() <= 30) {
+                        sender.sendMessage("Command is disabled as the server is about to restart!");
+                        return true;
+                    }
                 }
 
-                return true;
+                if (args.length == 0) { // Show regular balance top 10 from Essentials.
+                    sender.sendMessage("Users with the top balances (refreshes hourly):");
+                    java.util.List<java.util.Map.Entry<String, Integer>> topBalances = getTopBalances(10);
+
+                    if (topBalances.isEmpty()) {
+                        sender.sendMessage("No balance data available yet.");
+                        return true;
+                    }
+                    else {
+                        int rank = 1; //Ripped from LoginStreaks.
+                        for (java.util.Map.Entry<String, Integer> entry : topBalances) {
+                            String playerName = entry.getKey();
+                            int mostMoney = entry.getValue();
+
+                            sender.sendMessage(rank + ". " + playerName + ": $" + mostMoney);
+                            rank++;
+                        }
+
+                        return true;
+                    }
+                }
+
+                if (args[0].equalsIgnoreCase("businessbal") || args[0].equalsIgnoreCase("bbal")) { // Show BusinessBal top 10.
+                    if (plugin.isBusinessBalEnabled()) {
+                        sender.sendMessage("BusinessBals with the top balances (refreshes hourly):");
+
+                        java.util.List<java.util.Map.Entry<String, Double>> topBizBalances = getTopBusinessBals(10);
+
+                        if (topBizBalances.isEmpty()) {
+                            sender.sendMessage("No balance data available yet.");
+                            return true;
+                        }
+                        else {
+                            int rank = 1;
+                            for (java.util.Map.Entry<String, Double> entry : topBizBalances) {
+                                String accountName = entry.getKey();
+                                double mostMoney = entry.getValue();
+
+                                sender.sendMessage(rank + ". " + accountName + ": $" + mostMoney);
+                                rank++;
+                            }
+
+                            return true;
+                        }
+                    }
+                    else {
+                        sender.sendMessage("BusinessBal is not installed.");
+                        return true;
+                    }
+                }
+                else { // Show regular balance top 10 from Essentials.
+                    sender.sendMessage("Users with the top balances (refreshes hourly):");
+                    java.util.List<java.util.Map.Entry<String, Integer>> topBalances = getTopBalances(10);
+
+                    if (topBalances.isEmpty()) {
+                        sender.sendMessage("No balance data available yet.");
+                        return true;
+                    }
+                    else {
+                        int rank = 1; //Ripped from LoginStreaks.
+                        for (java.util.Map.Entry<String, Integer> entry : topBalances) {
+                            String playerName = entry.getKey();
+                            int mostMoney = entry.getValue();
+
+                            sender.sendMessage(rank + ". " + playerName + ": $" + mostMoney);
+                            rank++;
+                        }
+
+                        return true;
+                    }
+                }
             }
         }
         return true;
@@ -54,5 +192,12 @@ public class CommandBaltop implements CommandExecutor {
             return plugin.cachedTopBalances.subList(0, limit);
         }
         return plugin.cachedTopBalances;
+    }
+    public java.util.List<java.util.Map.Entry<String, Double>> getTopBusinessBals(int limit) {
+        // Return from cache instead of reading files
+        if (plugin.cachedTopBusinessBals.size() > limit) {
+            return plugin.cachedTopBusinessBals.subList(0, limit);
+        }
+        return plugin.cachedTopBusinessBals;
     }
 }
