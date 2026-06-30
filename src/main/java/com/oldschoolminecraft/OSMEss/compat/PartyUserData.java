56e5eb3212a5559e9aaf09d24b75bc2dd72b7cc0
SavageUser <LimitlessPowerHD@gmail.com>
2026-06-29 23:23:30 -0400

    Update PartyUserData.java

--- src/main/java/com/oldschoolminecraft/OSMEss/compat/PartyUserData.java
+++ src/main/java/com/oldschoolminecraft/OSMEss/compat/PartyUserData.java
@@ -1,7 +1,6 @@
 package com.oldschoolminecraft.OSMEss.compat;
 
 import com.google.gson.Gson;
-import org.bukkit.Location;
 
 import java.util.List;
 
@@ -10,7 +9,7 @@ public class PartyUserData {
     public static final Gson gson = new Gson();
     public String partyName;
     public String partyOwner;
-    public int dateCreated;
-    public SerializableLocation partyHomeLocation;
+    public long dateCreated;
+    public String partyHomeLocation;
     public List<String> partyMembers;
 }
